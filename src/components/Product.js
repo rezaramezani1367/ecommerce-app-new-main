775a02975c798c1c47e5efedc070da7a87c65cc2
reza ramezani <58149594+rezaramezani1367@users.noreply.github.com>
2022-11-07 13:14:28 +0330

    commit for component product section2

--- src/components/Product.js
+++ src/components/Product.js
@@ -12,6 +12,7 @@ import {
   Avatar,
   ListItemText,
   Divider,
+  ListItemIcon,
 } from "@mui/material";
 
 import React, { useEffect } from "react";
@@ -19,7 +20,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getProduct } from "../redux/action";
 import Grid from "@mui/material/Unstable_Grid2";
-
+import { Send, Album, StarOutline } from "@mui/icons-material";
+import { Box } from "@mui/system";
 
 const Product = () => {
   const { id } = useParams();
@@ -31,24 +33,215 @@ const Product = () => {
     dispatch(getProduct(id));
   }, []);
   console.log({ productLoading, productData, productError });
-  return (
-    <>
-      {productData.map((item, index) => (
-        <Grid container key={item._id} spacing={4}  >
-          <Grid xs={12} md={6}>
-            <img
-              src="https://mui.com/static/images/cards/contemplative-reptile.jpg"
-              alt={item.name}
-              className="border-2 rounded-md border-slate-400 w-full"
-            />
-          </Grid>
-          <Grid xs={12} md={6} className="p-4">
-           
-          </Grid>
-        </Grid>
-      ))}
-    </>
-  );
+  switch (true) {
+    case productLoading:
+      return <div>loading...</div>;
+    case Boolean(productError):
+      return <div>{productError}</div>;
+
+    default:
+      return (
+        <>
+          {productData.map((item, index) => (
+            <Grid container key={item._id} spacing={4}>
+              <Grid xs={12} md={6} className="relative">
+                <img
+                  src="https://mui.com/static/images/cards/contemplative-reptile.jpg"
+                  alt={item.name}
+                  className="border-2 rounded-md border-slate-400 w-full"
+                />
+                <Box className="absolute bottom-5 left-5 w-full px-4 text-yellow-500 z-10">
+                  <Rating
+                    size="small"
+                    name="text-feedback"
+                    defaultValue={item.rating}
+                    readOnly
+                    precision={0.25}
+                    sx={{
+                      fontSize: "1rem",
+                    }}
+                    emptyIcon={
+                      <StarOutline
+                        className="text-white"
+                        sx={{
+                          fontSize: "1rem",
+                        }}
+                      />
+                    }
+                  />
+                </Box>
+              </Grid>
+              <Grid xs={12} md={6} className="p-4">
+                <List
+                  sx={{
+                    bgcolor: "background.paper",
+                  }}
+                  className=""
+                >
+                  {/* Name */}
+                  <ListItem alignItems="flex-start">
+                    <ListItemIcon>
+                      <Album />
+                    </ListItemIcon>
+                    <ListItemText
+                      primary={
+                        <>
+                          <Typography
+                            variant="span"
+                            color="text.primary"
+                            className="inline font-bold text-xl  pr-2 "
+                          >
+                            Name:
+                          </Typography>
+                          <Typography
+                            variant="span"
+                            color="text.primary"
+                            className="inline text-lg  uppercase"
+                          >
+                            {item.name}
+                          </Typography>
+                        </>
+                      }
+                    />
+                  </ListItem>
+                  <Divider variant="inset" component="li" />
+                  {/* category */}
+                  <ListItem alignItems="flex-start">
+                    <ListItemIcon>
+                      <Album />
+                    </ListItemIcon>
+                    <ListItemText
+                      primary={
+                        <>
+                          <Typography
+                            variant="span"
+                            color="text.primary"
+                            className="inline font-bold text-xl  pr-2 "
+                          >
+                            Category:
+                          </Typography>
+                          <Typography
+                            variant="span"
+                            color="text.primary"
+                            className="inline text-lg  capitalize"
+                          >
+                            {item.category}
+                          </Typography>
+                        </>
+                      }
+                    />
+                  </ListItem>
+                  <Divider variant="inset" component="li" />
+                  <ListItem alignItems="flex-start">
+                    <ListItemIcon>
+                      <Album />
+                    </ListItemIcon>
+                    <ListItemText
+                      primary={
+                        <>
+                          <Typography
+                            variant="span"
+                            color="text.primary"
+                            className="inline font-bold text-xl  pr-2 "
+                          >
+                            Price:
+                          </Typography>
+                          <Typography
+                            variant="span"
+                            color="text.primary"
+                            className="inline text-lg "
+                          >
+                            {item.price.toFixed(2)}$
+                          </Typography>
+                        </>
+                      }
+                    />
+                  </ListItem>
+                  <Divider variant="inset" component="li" />
+                  <ListItem alignItems="flex-start">
+                    <ListItemIcon>
+                      <Album />
+                    </ListItemIcon>
+                    <ListItemText
+                      primary={
+                        <>
+                          <Typography
+                            variant="span"
+                            color="text.primary"
+                            className="inline font-bold text-xl  pr-2 "
+                          >
+                            Brand:
+                          </Typography>
+                          <Typography
+                            variant="span"
+                            color="text.primary"
+                            className="inline text-lg  capitalize"
+                          >
+                            {item.brand}
+                          </Typography>
+                        </>
+                      }
+                    />
+                  </ListItem>
+                  <Divider variant="inset" component="li" />
+                  <ListItem alignItems="flex-start">
+                    <ListItemIcon>
+                      <Album />
+                    </ListItemIcon>
+                    <ListItemText
+                      primary={
+                        <>
+                          <Typography
+                            variant="span"
+                            color="text.primary"
+                            className="inline font-bold text-xl  pr-2 capitalize"
+                          >
+                            count In Stock:
+                          </Typography>
+                          <Typography
+                            variant="span"
+                            color="text.primary"
+                            className="inline text-lg  capitalize"
+                          >
+                            {item.countInStock}
+                          </Typography>
+                        </>
+                      }
+                    />
+                  </ListItem>
+                  <Divider variant="inset" component="li" />
+                  <ListItem alignItems="flex-start">
+                    <ListItemIcon>
+                      <Album />
+                    </ListItemIcon>
+                    <ListItemText
+                      primary={
+                        <>
+                          <Typography
+                            variant="span"
+                            color="text.primary"
+                            className="inline font-bold text-xl  pr-2 capitalize"
+                          >
+                            color:
+                          </Typography>
+                          <Typography
+                            variant="span"
+                            color="text.primary"
+                            className="inline text-lg  capitalize"
+                          >
+                            {item.color}
+                          </Typography>
+                        </>
+                      }
+                    />
+                  </ListItem>
+                </List>
+              </Grid>
+            </Grid>
+          ))}
+        </>
+      );
+  }
 };
 
 export default Product;

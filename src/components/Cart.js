ffb13f0b6da8c7f5c10a0dfb1e63c0711c9ce615
reza ramezani <58149594+rezaramezani1367@users.noreply.github.com>
2022-11-11 12:59:30 +0330

    cart edit

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -9,176 +9,200 @@ import {
   IconButton,
 } from "@mui/material";
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart, minusFromCart, removeItmeCart } from "../redux/actionCart";
+
+
+
 
 const Cart = () => {
-  return (
-    <Box className=" flex w-full flex-col">
-      <Paper
-        elevation={4}
-        className=""
-        sx={{ display: { xs: "none", md: "block", borderRadius: 0 } }}
-      >
-        <Grid
-          container
-          gap={1}
-          padding={1}
-          alignItems="center"
-          className="text-center font-bold text-xl capitalize"
-        >
-          <Grid sx={{ width: { xs: 200, md:170 } }}>Image</Grid>
-          <Grid  md={3}>
-            Name
-          </Grid>
-          <Grid md>Price</Grid>
-          <Grid  md={3}>
-            count
-          </Grid>
-          <Grid md>SubTotal</Grid>
-        </Grid>
-      </Paper>
-      <Paper
-        elevation={4}
-        sx={{ borderRadius: 0, textAlign: { md: "left", md: "center" } }}
-        className="overflow-hidden p-2 md:p-0 relative"
-      >
-        <Grid
-          container
-          padding={1}
-          sx={{ alignItems: { xs: "start", md: "center" }, gap: { xs: 1 } }}
-        >
-          <Grid
-            sx={{ width: { xs: 200, md: 170 }, height: 100 }}
-            
-          >
-           <Box className="w-full h-full flex items-center">
-           <img
-              src="https://mui.com/static/images/cards/contemplative-reptile.jpg"
-              alt="11"
-              className="max-h-full max-w-full"
-              onError={({ currentTarget }) => {
-                currentTarget.onerror = null; // prevents looping
-                currentTarget.src =
-                  "https://mui.com/static/images/cards/contemplative-reptile.jpg";
-              }}
-            />
-           </Box>
-          </Grid>
-          <Grid xs={12} sm={6} md={3}>
-            <Stack
-              direction="row"
-              gap={1}
-              alignItems="center"
-              sx={{ justifyContent: { sm: "start", md: "center" } }}
-              className="capitalize"
-            >
-              <Typography
-                variant="span"
-                className="font-bold border-r pr-2 text-lg"
-                sx={{ display: { xs: "block", sm: "none" } }}
-              >
-                Name
-              </Typography>
-              <Typography variant="span" className="text-lg">
-                Sony
-              </Typography>
-            </Stack>
-          </Grid>
-
-          <Grid xs={12} sm={3} md sx={{ alignSelf: "center" }}>
-            <Stack
-              direction="row"
-              gap={1}
-              alignItems="center"
-              sx={{ justifyContent: { sm: "start", md: "center" } }}
-              className="capitalize"
-            >
-              <Typography
-                variant="span"
-                className="font-bold border-r pr-2 text-lg"
-                sx={{ display: { xs: "block", md: "none" } }}
-              >
-                Price
-              </Typography>
-              <Typography variant="span" className="text-lg ">
-                1000.2$
-              </Typography>
-            </Stack>
-          </Grid>
-          <Grid xs={12} sm={4} md={3}>
-            <Box
-              className="flex justify-center"
-              sx={{ justifyContent: { xs: "start", sm: "center" } }}
-            >
-              <Paper
-                elevation={4}
-                className="flex justify-between items-center overflow-hidden"
-              >
-                <Button
-                  size="small"
-                  disableElevation
-                  disableRipple
-                  disableFocusRipple
-                  sx={{ borderRadius: "0", minWidth: 40 }}
-                >
-                  <Remove sx={{ color: "red" }} />
-                </Button>
-                <Stack>
-                  <Typography
-                    variant="span"
-                    className="w-12 min-h-full font-bold text-center text-lg"
-                  >
-                    12
-                  </Typography>
-                  {/* <Typography
-                    variant="span"
-                    className="w-12 min-h-full font-bold text-center text-red-500"
-                  >
-                    max
-                  </Typography> */}
-                </Stack>
-                <Button
-                  disableElevation
-                  disableRipple
-                  disableFocusRipple
-                  sx={{ borderRadius: "0", minWidth: 40 }}
-                >
-                  <Add fontSize="medium" />
-                </Button>
-              </Paper>
-            </Box>
-          </Grid>
-          <Grid xs={12} sm={4} md sx={{ alignSelf: "center" }}>
-            <Stack
-              direction="row"
-              gap={1}
-              alignItems="center"
-              sx={{ justifyContent: { sm: "start", md: "center" } }}
-              className="capitalize"
-            >
-              <Typography
-                variant="span"
-                className="font-bold border-r pr-2 text-lg"
-                sx={{ display: { xs: "block", md: "none" } }}
-              >
-                SubTotal
-              </Typography>
-              <Typography variant="span" className="text-lg text-red-500">
-                1000.2$
-              </Typography>
-            </Stack>
-          </Grid>
-        </Grid>
-        <Box className="absolute top-1 right-1">
-          <IconButton aria-label="delete" color="error" size="small">
-            <Close fontSize="small" />
-          </IconButton>
+  const {
+    cart: { cartLoading, cartData, cartError },
+  } = useSelector((last) => last);
+  const dispatch = useDispatch();
+  switch (true) {
+    case Boolean(!cartData.length):
+      return (
+        <Box className="flex justify-center items-center  text-red-600 font-bold h-96">
+          <Typography variant="h3" component="span">
+            Cart Is Empty
+          </Typography>
         </Box>
-      </Paper>
-     
+      );
 
-     
-    </Box>
-  );
+    default:
+      return (
+        <Box className=" flex w-full flex-col">
+          <Paper
+            elevation={4}
+            className=""
+            sx={{ display: { xs: "none", md: "block", borderRadius: 0 } }}
+          >
+            <Grid
+              container
+              gap={1}
+              padding={1}
+              alignItems="center"
+              className="text-center font-bold text-xl capitalize"
+            >
+              <Grid sx={{ width: { xs: 200, md: 170 } }}>Image</Grid>
+              <Grid md={3}>Name</Grid>
+              <Grid md>Price</Grid>
+              <Grid md={3}>count</Grid>
+              <Grid md>SubTotal</Grid>
+            </Grid>
+          </Paper>
+          {cartData.map((item, index) => (
+            <Paper
+              key={item._id}
+              elevation={4}
+              sx={{ borderRadius: 0, textAlign: { md: "left", md: "center" } }}
+              className="overflow-hidden p-2 md:p-0 relative"
+            >
+              <Grid
+                container
+                padding={1}
+                sx={{
+                  alignItems: { xs: "start", md: "center" },
+                  gap: { xs: 1 },
+                }}
+              >
+                <Grid sx={{ width: { xs: 200, md: 170 }, height: 100 }}>
+                  <Box className="w-full h-full flex items-center">
+                    <img
+                      src={item.image}
+                      alt="11"
+                      className="max-h-full max-w-full"
+                      onError={({ currentTarget }) => {
+                        currentTarget.onerror = null; // prevents looping
+                        currentTarget.src =
+                          "https://mui.com/static/images/cards/contemplative-reptile.jpg";
+                      }}
+                    />
+                  </Box>
+                </Grid>
+                <Grid xs={12} sm={6} md={3}>
+                  <Stack
+                    direction="row"
+                    gap={1}
+                    alignItems="center"
+                    sx={{ justifyContent: { sm: "start", md: "center" } }}
+                    className="capitalize"
+                  >
+                    <Typography
+                      variant="span"
+                      className="font-bold border-r pr-2 text-lg"
+                      sx={{ display: { xs: "block", sm: "none" } }}
+                    >
+                      Name
+                    </Typography>
+                    <Typography variant="span" className="text-lg">
+                      {item.name}
+                    </Typography>
+                  </Stack>
+                </Grid>
+
+                <Grid xs={12} sm={3} md sx={{ alignSelf: "center" }}>
+                  <Stack
+                    direction="row"
+                    gap={1}
+                    alignItems="center"
+                    sx={{ justifyContent: { sm: "start", md: "center" } }}
+                    className="capitalize"
+                  >
+                    <Typography
+                      variant="span"
+                      className="font-bold border-r pr-2 text-lg"
+                      sx={{ display: { xs: "block", md: "none" } }}
+                    >
+                      Price
+                    </Typography>
+                    <Typography variant="span" className="text-lg ">
+                      {item.price.toFixed(2)}$
+                    </Typography>
+                  </Stack>
+                </Grid>
+                <Grid xs={12} sm={4} md={3}>
+                  <Box
+                    className="flex justify-center"
+                    sx={{ justifyContent: { xs: "start", sm: "center" } }}
+                  >
+                    <Paper
+                      elevation={4}
+                      className="flex justify-between items-center overflow-hidden"
+                    >
+                      <Button
+                        size="small"
+                        disableElevation
+                        disableRipple
+                        disableFocusRipple
+                        sx={{ borderRadius: "0", minWidth: 40 }}
+                        onClick={() => dispatch(minusFromCart(item))}
+                      >
+                        <Remove sx={{ color: "red" }} />
+                      </Button>
+                      <Stack>
+                        <Typography
+                          variant="span"
+                          className="w-12 min-h-full font-bold text-center text-lg"
+                        >
+                          {item.quantity}
+                        </Typography>
+                        {/* <Typography
+                      variant="span"
+                      className="w-12 min-h-full font-bold text-center text-red-500"
+                    >
+                      max
+                    </Typography> */}
+                      </Stack>
+                      <Button
+                        disableElevation
+                        disableRipple
+                        disableFocusRipple
+                        sx={{ borderRadius: "0", minWidth: 40 }}
+                        onClick={() => dispatch(addToCart(item))}
+                      >
+                        <Add fontSize="medium" />
+                      </Button>
+                    </Paper>
+                  </Box>
+                </Grid>
+                <Grid xs={12} sm={4} md sx={{ alignSelf: "center" }}>
+                  <Stack
+                    direction="row"
+                    gap={1}
+                    alignItems="center"
+                    sx={{ justifyContent: { sm: "start", md: "center" } }}
+                    className="capitalize"
+                  >
+                    <Typography
+                      variant="span"
+                      className="font-bold border-r pr-2 text-lg"
+                      sx={{ display: { xs: "block", md: "none" } }}
+                    >
+                      SubTotal
+                    </Typography>
+                    <Typography variant="span" className="text-lg text-red-500">
+                      {(item.price * item.quantity).toFixed(2)}$
+                    </Typography>
+                  </Stack>
+                </Grid>
+              </Grid>
+              <Box className="absolute top-1 right-1">
+                <IconButton
+                  aria-label="delete"
+                  color="error"
+                  size="small"
+                  onClick={() => dispatch(removeItmeCart(index))}
+                >
+                  <Close fontSize="small" />
+                </IconButton>
+              </Box>
+            </Paper>
+          ))}
+        </Box>
+      );
+  }
 };
 
 export default Cart;
--- src/redux/actionCart.js
+++ src/redux/actionCart.js
@@ -1,29 +1,153 @@
 import axios from "axios";
+import Swal from "sweetalert2";
 import { cartLoading, cartSuccess, cartError } from "./constants";
 const client = axios.create({
-    baseURL: "http://kzico.runflare.run",
+  baseURL: "http://kzico.runflare.run",
+});
+
+const Toast = Swal.mixin({
+  toast: true,
+  position: "bottom-start",
+  showConfirmButton: false,
+  timer: 1500,
+
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener("mouseenter", Swal.stopTimer);
+    toast.addEventListener("mouseleave", Swal.resumeTimer);
+  },
+});
+
+export const addToCart = (item) => (dispatch, getState) => {
+  console.log(item);
+  dispatch({
+    type: cartLoading,
+    payload: { ...getState().cart, cartLoading: true },
   });
-
-  export const addToCart = (item) =>  (dispatch, getState) => {
-    dispatch({
-      type: cartLoading,
-      payload: { ...getState().cart, cartLoading: true },
-    });
-    try {
-        const {cartData}=getState().cart;
-        console.log(cartData);
-
-
-
-        // dispatch({
-        //   type: cartSuccess,
-        //   payload: { cartLoading: false,cartData:[...data], cartError:''},
-        // });
-      // console.log(data)
-    } catch (error) {
+  try {
+    const { cartData } = getState().cart;
+    const currentDataCart = cartData.filter((itm) => itm._id == item._id);
+    if (currentDataCart.length) {
+      const help = cartData.map((itm) => {
+        if (itm._id == item._id) {
+          return { ...itm, quantity: itm.quantity + 1 };
+        } else {
+          return itm;
+        }
+      });
       dispatch({
-          type: cartError,
-          payload: { cartLoading: false,cartData:[], cartError:error.message},
-        });
+        type: cartSuccess,
+        payload: {
+          cartData: [...help],
+          cartLoading: false,
+          cartError: "",
+        },
+      });
+      localStorage.setItem("cart", JSON.stringify(help));
+    } else {
+      cartData.push({ ...item, quantity: 1 });
+      dispatch({
+        type: cartSuccess,
+        payload: {
+          cartData: [...cartData],
+          cartLoading: false,
+          cartError: "",
+        },
+      });
+      localStorage.setItem("cart", JSON.stringify(cartData));
     }
-  };
+  } catch (error) {
+    dispatch({
+      type: cartError,
+      payload: { cartLoading: false, cartData: [], cartError: error.message },
+    });
+  }
+};
+export const minusFromCart = (item) => (dispatch, getState) => {
+  dispatch({
+    type: cartLoading,
+    payload: { ...getState().cart, cartLoading: true },
+  });
+  try {
+    const { cartData } = getState().cart;
+    const currentDataCart = cartData.filter((itm) => itm._id == item._id);
+    console.log(currentDataCart);
+    if (currentDataCart.length && currentDataCart[0].quantity > 1) {
+      const help = cartData.map((itm) => {
+        if (itm._id == item._id) {
+          return { ...itm, quantity: itm.quantity - 1 };
+        } else {
+          return itm;
+        }
+      });
+      dispatch({
+        type: cartSuccess,
+        payload: {
+          cartData: [...help],
+          cartLoading: false,
+          cartError: "",
+        },
+      });
+      localStorage.setItem("cart", JSON.stringify(help));
+    } else {
+      let help = cartData.filter((itm) => itm._id !== item._id);
+      dispatch({
+        type: cartSuccess,
+        payload: {
+          cartData: [...help],
+          cartLoading: false,
+          cartError: "",
+        },
+      });
+      if (help.length) {
+        localStorage.setItem("cart", JSON.stringify(help));
+      } else {
+        localStorage.removeItem("cart");
+        Toast.fire({
+          icon: "error",
+          title: `${item.name}  deleted from cart successfully`,
+        });
+      }
+    }
+  } catch (error) {
+    dispatch({
+      type: cartError,
+      payload: { cartLoading: false, cartData: [], cartError: error.message },
+    });
+  }
+};
+export const removeItmeCart = (index) => (dispatch, getState) => {
+  dispatch({
+    type: cartLoading,
+    payload: { ...getState().cart, cartLoading: true },
+  });
+  try {
+    const { cartData } = getState().cart;
+
+    cartData.splice(index, 1);
+    console.log({cartData,index,},cartData.length)
+
+    dispatch({
+      type: cartSuccess,
+      payload: {
+        cartData: [...cartData],
+        cartLoading: false,
+        cartError: "",
+      },
+    });
+    Toast.fire({
+      icon: "error",
+      title: `${cartData[index].name}  deleted from cart successfully`,
+    });
+    if (cartData.length) {
+      localStorage.setItem("cart", JSON.stringify(cartData));
+    } else {
+      localStorage.removeItem("cart");
+    }
+  } catch (error) {
+    dispatch({
+      type: cartError,
+      payload: { cartLoading: false, cartData: [], cartError: error.message },
+    });
+  }
+};

26f96879627aaa8d05aaad8bb88f7c2368037444
reza ramezani <58149594+rezaramezani1367@users.noreply.github.com>
2022-11-13 15:53:25 +0330

    commit for ui component login/signup

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,9 @@
-import { LoginOutlined } from "@mui/icons-material";
-import { TabContext, TabList, TabPanel } from "@mui/lab";
+import { LoginOutlined,Login as LoginIcon, Portrait, Key, Email } from "@mui/icons-material";
+
 import {
   Box,
   Button,
+  InputAdornment,
   Paper,
   styled,
   Tab,
@@ -11,52 +12,51 @@ import {
 } from "@mui/material";
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import Signup from "./Signup";
 
 export const CustomField = styled(TextField)(() => ({
   "& label": {
     fontWeight: 500,
-    top: 3,
-    left: 10,
+    top: 0,
+    left: 0,
   },
   "& label.Mui-focused": {
     top: 0,
     left: 0,
   },
+  "& label.MuiFormLabel-filled": {
+    top: 0,
+    left: 0,
+  },
   "& input": {
-    padding: "0.75rem 2rem",
+    padding: "0.75rem 0.5rem",
   },
   "& .MuiOutlinedInput-root": {
     "& fieldset": {
       borderColor: "#717D7E",
       borderRadius: "3rem",
-    },
-    "&:hover fieldset": {
-      // borderColor: "blue",
-    },
+    }
   },
 }));
 export const HeaderLogin = ({ value }) => {
   const navigate = useNavigate();
   return (
-    <Box sx={{ borderBottom: 1, borderColor: "divider"}}>
-      <Tabs value={value} aria-label="basic tabs">
-        <Tab label="Login" value="1" onClick={()=>navigate('/login')} />
-        <Tab label="Signup" value="2" onClick={()=>navigate('/signup')} />
+    <Box sx={{ borderBottom: 1, borderColor: "divider",overflow:'hidden' }}>
+      <Tabs value={value} aria-label="basic tabs" sx={{bgcolor: ''}}>
+        <Tab icon={<LoginIcon />} iconPosition='start' label="Login" value="1" onClick={() => navigate("/login")} />
+        <Tab icon={<Portrait />}  iconPosition='start' label="Signup" value="2" onClick={() => navigate("/signup")} />
       </Tabs>
     </Box>
   );
 };
 const Login = () => {
-
-
   return (
     <>
-      <Box
-        className="flex justify-center items-center font-bold text-2xl "
-        sx={{ fontWeight: "700" }}
-      >
-        <Paper className=" w-full md:w-150 " elevation={4}>
+      <Box className="flex justify-center items-center">
+        <Paper
+          className=" w-full md:w-150 overflow-hidden"
+          sx={{ borderRadius: "1.8rem" }}
+          elevation={4}
+        >
           <HeaderLogin value="1" />
           <Box
             component="form"
@@ -68,17 +68,33 @@ const Login = () => {
               label="Email"
               id="outlined-username-small"
               // defaultValue="Small"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <Email />
+                  </InputAdornment>
+                ),
+              }}
 
               size="small"
               fullWidth
             />
 
             <CustomField
+            startIcon={<LoginIcon />}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Key />
+                </InputAdornment>
+              ),
+            }}
               label="Password"
               id="outlined-username-small"
               // defaultValue="Small"
               size="small"
               fullWidth
+              type="password"
             />
 
             <Box className="text-center">
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,8 +1,16 @@
-import { LoginOutlined, Portrait } from "@mui/icons-material";
+import { AccountCircle, Email, Key, LoginOutlined, PhoneAndroid, Portrait } from "@mui/icons-material";
 import { TabContext, TabList, TabPanel } from "@mui/lab";
-import { Box, Button, Paper, Tab, Tabs, TextField } from "@mui/material";
+import {
+  Box,
+  Button,
+  InputAdornment,
+  Paper,
+  Tab,
+  Tabs,
+  TextField,
+} from "@mui/material";
 import React from "react";
-import { CustomField,HeaderLogin } from "./Login";
+import { CustomField, HeaderLogin } from "./Login";
 
 const Signup = () => {
   return (
@@ -10,7 +18,11 @@ const Signup = () => {
       className="flex justify-center items-center font-bold text-2xl"
       sx={{ fontWeight: "700" }}
     >
-      <Paper className=" w-full md:w-150 " elevation={4}>
+      <Paper
+        className=" w-full md:w-150 overflow-hidden"
+        sx={{ borderRadius: "1.8rem" }}
+        elevation={4}
+      >
         <HeaderLogin value="2" />
         <Box
           component="form"
@@ -22,24 +34,56 @@ const Signup = () => {
             helperText="Please enter your name"
             label="Username"
             id="outlined-username-small"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <AccountCircle />
+                </InputAdornment>
+              ),
+            }}
             size="small"
             fullWidth
           />
           <CustomField
             label="Email"
             id="outlined-username-small"
+            // defaultValue="Small"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Email />
+                </InputAdornment>
+              ),
+            }}
             size="small"
             fullWidth
           />
+
           <CustomField
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Key />
+                </InputAdornment>
+              ),
+            }}
             label="Password"
             id="outlined-username-small"
+            // defaultValue="Small"
             size="small"
             fullWidth
-          />
+            type="password"
+            />
           <CustomField
             label="Mobile"
             id="outlined-username-small"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <PhoneAndroid />
+                </InputAdornment>
+              ),
+            }}
             size="small"
             fullWidth
           />
